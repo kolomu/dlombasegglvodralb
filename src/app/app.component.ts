f76620cd1c9903b0fc53b5b3b8fb63d96ef53d4e
Kevin Olomu <kevin.olomu@posteo.de>
2023-02-15 07:34:24 +0100

    installed font-awesome and sample navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dlombasegglvodralb';
+
+  faCoffee = faCoffee;
+
+  menuShow = false;
+  toggleNavbar(){
+    this.menuShow = !this.menuShow;
+  }
+
 }
